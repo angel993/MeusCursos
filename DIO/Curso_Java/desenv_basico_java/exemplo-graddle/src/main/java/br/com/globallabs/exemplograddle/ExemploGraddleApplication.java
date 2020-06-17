684e825c602d898602efbeacb871db13681c6dbc
romulopuk <pukpen@gmail.com>
2020-06-16 22:28:01 -0300

    Commit aulas java basica inicio

---
+++ DIO/Curso_Java/desenv_basico_java/exemplo-graddle/src/main/java/br/com/globallabs/exemplograddle/ExemploGraddleApplication.java
@@ -0,0 +1,13 @@
+package br.com.globallabs.exemplograddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploGraddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploGraddleApplication.class, args);
+	}
+
+}
---
+++ DIO/Curso_Java/desenv_basico_java/exemplo-graddle/src/test/java/br/com/globallabs/exemplograddle/ExemploGraddleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.globallabs.exemplograddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGraddleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
